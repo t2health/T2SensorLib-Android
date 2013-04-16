4719dfc79510ef045f95548a27164c2aee7e8b10
Scott Coleman <scott.coleman@tee2.org>
2013-04-16 13:37:35 -0700

    Formatting and commenting updates

--- src/com/t2/t2sensorlib/BigBrotherService.java
+++ src/com/t2/t2sensorlib/BigBrotherService.java
@@ -1,3 +1,30 @@
+/* T2AndroidLib-SG for Signal Processing
+ * 
+ * Copyright © 2009-2012 United States Government as represented by 
+ * the Chief Information Officer of the National Center for Telehealth 
+ * and Technology. All Rights Reserved.
+ * 
+ * Copyright © 2009-2012 Contributors. All Rights Reserved. 
+ * 
+ * THIS OPEN SOURCE AGREEMENT ("AGREEMENT") DEFINES THE RIGHTS OF USE, 
+ * REPRODUCTION, DISTRIBUTION, MODIFICATION AND REDISTRIBUTION OF CERTAIN 
+ * COMPUTER SOFTWARE ORIGINALLY RELEASED BY THE UNITED STATES GOVERNMENT 
+ * AS REPRESENTED BY THE GOVERNMENT AGENCY LISTED BELOW ("GOVERNMENT AGENCY"). 
+ * THE UNITED STATES GOVERNMENT, AS REPRESENTED BY GOVERNMENT AGENCY, IS AN 
+ * INTENDED THIRD-PARTY BENEFICIARY OF ALL SUBSEQUENT DISTRIBUTIONS OR 
+ * REDISTRIBUTIONS OF THE SUBJECT SOFTWARE. ANYONE WHO USES, REPRODUCES, 
+ * DISTRIBUTES, MODIFIES OR REDISTRIBUTES THE SUBJECT SOFTWARE, AS DEFINED 
+ * HEREIN, OR ANY PART THEREOF, IS, BY THAT ACTION, ACCEPTING IN FULL THE 
+ * RESPONSIBILITIES AND OBLIGATIONS CONTAINED IN THIS AGREEMENT.
+ * 
+ * Government Agency: The National Center for Telehealth and Technology
+ * Government Agency Original Software Designation: T2AndroidLib1021
+ * Government Agency Original Software Title: T2AndroidLib for Signal Processing
+ * User Registration Requested. Please send email 
+ * with your contact information to: robert.kayl2@us.army.mil
+ * Government Agency Point of Contact for Original Software: robert.kayl2@us.army.mil
+ * 
+ */
 package com.t2.t2sensorlib;
 
 import android.app.ActivityManager;
@@ -58,8 +85,6 @@ import com.example.t2sensorlib.R;
 //import com.t2.bigbrother.R;
 import com.t2.t2sensorlib.Receiver.OnBioFeedbackMessageRecievedListener;
 
-
-
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
@@ -90,6 +115,7 @@ public class BigBrotherService extends Service implements SensorEventListener, L
 	
 	/**
 	 * Static names dealing with the external database
+	 * Note that currently these are not used by the DataOutHandler
 	 */
 	public static final String dDatabaseName = "";
 	public static final String dDesignDocName = "bigbrother-local";
@@ -97,7 +123,6 @@ public class BigBrotherService extends Service implements SensorEventListener, L
 	public static final String byDateViewName = "byDate";
 
 	private static final String mAppId = "BigBrotherService";	
-	
 	
 	private String mLastSentSMSMd5 = "";
 	/**
